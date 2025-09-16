105ea3920675218f7a57752db1cfd1362ed9dab7
Uche Ozoemena <codewithoz@outlook.com>
2025-09-16 22:12:56 +0100

    feat: run sync func in threadpool

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 from time import sleep
 from fastapi import FastAPI
 from fastapi.background import BackgroundTasks
+from fastapi.concurrency import run_in_threadpool
 import logging
 
 
@@ -14,17 +15,17 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 
 
-async def upload_to_s3():
+def upload_to_s3():
     # assume the upload will run synchronously for 5 seconds
     logger.info("Before sleep")
-    # sleep(5)
-    await asyncio.sleep(5)
+    sleep(5)
+    # await asyncio.sleep(5)
     logger.info("After sleep")
 
 
 @app.get("/process-files")
 async def process_files(background_tasks: BackgroundTasks):
     logger.info("Before process files")
-    background_tasks.add_task(upload_to_s3)
+    background_tasks.add_task(run_in_threadpool, upload_to_s3)
     logger.info("After process files")
     return {"ok": True}

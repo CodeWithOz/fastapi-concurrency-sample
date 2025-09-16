fbe5faca425056ab8899fb48b6818a4a00c1aed0
Uche Ozoemena <codewithoz@outlook.com>
2025-09-16 17:26:11 +0100

    feat: create blocking endpoint

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from python-async!")
+from time import sleep
+from fastapi import FastAPI
+import logging
 
 
-if __name__ == "__main__":
-    main()
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+
+
+async def upload_to_s3():
+    # assume the upload will take 5 seconds
+    sleep(5)
+
+
+@app.get("/process-files")
+async def process_files():
+    logger.info("Before process files")
+    await upload_to_s3()
+    logger.info("After process files")
